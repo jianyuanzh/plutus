56c55d1a4e4f200ebcef6673615b4113f5cabfe8
rooter <me@yflog.com>
2016-09-04 18:22:21 +0800

    #7 create tables for tags, buckets, money flow

--- src/main/resources/create_db.sql
+++ src/main/resources/create_db.sql
@@ -8,3 +8,35 @@ CREATE TABLE `plutus`.`users` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
   UNIQUE INDEX `userscol_UNIQUE` (`email` ASC));
+
+CREATE TABLE `plutus`.`tags` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NULL,
+  `value` VARCHAR(128) NULL,
+  `flowType` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
+
+CREATE TABLE `plutus`.`buckets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  `desc` VARCHAR(512) NULL,
+  `balance` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
+
+  CREATE TABLE `money_flows` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `flowType` int(11) DEFAULT NULL,
+  `desc` varchar(512) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `tagIds` varchar(45) DEFAULT NULL,
+  `fromBucketId` int(11) DEFAULT NULL,
+  `toBucketId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bucketId_idx` (`fromBucketId`),
+  KEY `toBucketId_idx` (`toBucketId`),
+  CONSTRAINT `fromBucketId` FOREIGN KEY (`fromBucketId`) REFERENCES `buckets` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `toBucketId` FOREIGN KEY (`toBucketId`) REFERENCES `buckets` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
